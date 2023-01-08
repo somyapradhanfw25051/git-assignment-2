a12ef8f12344de994af03b0dd609a327006d294a
somyapradhanfw25051 <121215804+somyapradhanfw25051@users.noreply.github.com>
2023-01-08 22:25:38 +0530

    Add files via upload

---
+++ ga1.js
@@ -0,0 +1,16 @@
+console.log(" 1 is independant number");
+for(let i=2;i<=10;i++){
+  let num = i;
+  let count=0;
+  for(let j=2; j<=num;j++){
+    if(num%j==0){
+    count++;
+  }
+}
+if(count==1){
+  console.log(i,"is a prime number");
+}
+else{
+  console.log(i,"not a prime number");
+}
+}
